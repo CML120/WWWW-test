dd16548651234f1f90d21f279853ec0919db229c
CML120 <cheelor922@gmail.com>
2023-04-18 18:48:06 -0500

    working token code for spotify query. card display sample code added.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,26 +1,108 @@
 var searchFormEl = document.querySelector('#search-form');
 var searchBtnEl = document.querySelector('#searchBtn')
 var searchInput = ''
+var client_id = 'b8a40684aaf24623a0845d2de7d55422';
+var client_secret = '059928fa94f647a3ad310fbb22d30473';
 // var resultCards = document.getElementById('result-cards');
+// var cardDisplayEl = document.querySelector('#card-display');
+var artistName = "The Weeknd";  // this will store the artist name from the TicketMaster search query then pass it to the Spotify query
+
+$('#searchBtn').on("click",function(event) {
+  event.preventDefault();
+
+// cannot remove is-hidden class   currently remove all class attributes, then re-add back using set attribute
+// cardDisplayEl.removeAttribute("class")
+// cardDisplayEl.setAttribute("class", "columns is-size-5 is-justify-content-space-evenly");
 
 
-
-
-function searchApiTM(){
-    var tmQueryUrl ='https://app.ticketmaster.com/discovery/v2/events?apikey=aSqzhWcc1ZxQnMoClqxGxcTRL8GrgFyA'
-
-    fetch(tmQueryUrl)
-    .then(function (response) {
+      // working fetch statements for obtaining token/ making first query
+      fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: 'grant_type=client_credentials&client_id=' + client_id + '&client_secret=' + client_secret
+      }).then(function (response) {
         return response.json();
       })
-      .then(function (data) {
-        console.log(data);
-      });
+        .then(function (data) {
 
-}
+          //reminder to remove console logs
+          console.log(data);
+          console.log(data.access_token)
+          var spotifyBearerToken = data.access_token;
+          console.log(spotifyBearerToken);
+          
+      
+          //need to nest this fetch statement so that the bearer token is valid/recognized due to scoping
+          // calling the api query outside of this fetch will create an unrecognized bearer token
+          fetch('https://api.spotify.com/v1/search?query=' + artistName + '&type=artist&market=us&limit=10&offset=0', {
+            headers: { 'Authorization': `Bearer ${spotifyBearerToken}` }
+          }).then(function (response) {
+            return response.json();
+          })
+            .then(function (data) {
+              //reminder to remove console logs
+              console.log(data);
+              console.log(data.artists.items[0].external_urls);
+              var hrefLink = data.artists.items[0].external_urls;
+             
+              console.log(hrefLink.spotify);
 
-// var client_id = 'b8a40684aaf24623a0845d2de7d55422';
-// var client_secret = '059928fa94f647a3ad310fbb22d30473';
+              //functional example code for displaying results cards
+              //using template literal to add event cards
+              for(i = 0; i <= 3; i++){
+
+                var cardDisplay =`
+              <div class="card ">
+                <ul>
+                    <li id="event1">`+ "Insert Event Here" + `</li>
+                    <li id="artist1">`+ "Insert Artist name here" + `</li>
+                    <li id="date1">`+ "Insert Date here" + `</li>
+                    <li id="time1">`+ "Insert Time here" + `</li>
+                    <li id="location1">`+ "Insert location here" + `</li>
+                    <li id="link1"> <a href=" `+ hrefLink.spotify + `" target="_blank">` + artistName + " Spotify Playlist" + ` </a> </li>
+                </ul>
+              </div>
+              `;
+
+              $("#card-display").append(cardDisplay);
+
+              }
+              
+            });
+      
+        });
+
+
+})
+
+
+
+
+// ticket master query >  finds event based on artist search
+  //  artist search result > passed into string
+    // nest spotify fetch > receive string for "artist" from ticket master fetch
+      // spotify > get bearer token
+        // pass bearer token + artist into spotify query
+          // display link
+
+
+
+// function searchApiTM(){
+//     var tmQueryUrl ='https://app.ticketmaster.com/discovery/v2/events?apikey=aSqzhWcc1ZxQnMoClqxGxcTRL8GrgFyA'
+
+//     fetch(tmQueryUrl)
+//     .then(function (response) {
+//         return response.json();
+//       })
+//       .then(function (data) {
+//         console.log(data);
+//       });
+
+// }
+
+
 // fetch('https://accounts.spotify.com/api/token', {
 //   method: 'POST',
 //   headers: {
@@ -73,32 +155,32 @@ function searchApiTM(){
       }
 
 
-      // working fetch statements for obtaining token/ making first query
-  fetch('https://accounts.spotify.com/api/token', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    body: 'grant_type=client_credentials&client_id=' + client_id + '&client_secret=' + client_secret
-  }).then(function (response) {
-    return response.json();
-  })
-    .then(function (data) {
-      console.log(data);
-      console.log(data.access_token)
-      var spotifyBearerToken = data.access_token;
-      console.log(spotifyBearerToken);
+  //     // working fetch statements for obtaining token/ making first query
+  // fetch('https://accounts.spotify.com/api/token', {
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/x-www-form-urlencoded'
+  //   },
+  //   body: 'grant_type=client_credentials&client_id=' + client_id + '&client_secret=' + client_secret
+  // }).then(function (response) {
+  //   return response.json();
+  // })
+  //   .then(function (data) {
+  //     console.log(data);
+  //     console.log(data.access_token)
+  //     var spotifyBearerToken = data.access_token;
+  //     console.log(spotifyBearerToken);
       
   
-      //need to nest this fetch statement so that the bearer token is valid/recognized due to scoping
-      // calling the api query outside of this fetch will create an unrecognized bearer token
-      fetch('https://api.spotify.com/v1/artists/4Z8W4fKeB5YxbusRsdQVPb', {
-        headers: { 'Authorization': `Bearer ${spotifyBearerToken}` }
-      }).then(function (response) {
-        return response.json();
-      })
-        .then(function (data) {
-          console.log(data);
-        });
+  //     //need to nest this fetch statement so that the bearer token is valid/recognized due to scoping
+  //     // calling the api query outside of this fetch will create an unrecognized bearer token
+  //     fetch('https://api.spotify.com/v1/artists/4Z8W4fKeB5YxbusRsdQVPb', {
+  //       headers: { 'Authorization': `Bearer ${spotifyBearerToken}` }
+  //     }).then(function (response) {
+  //       return response.json();
+  //     })
+  //       .then(function (data) {
+  //         console.log(data);
+  //       });
   
-    });
+  //   });
